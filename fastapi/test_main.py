99b4940edffc6c4ae79474c283164f313df1ef84
SergiLoPa <sergiolopezparejo@gmail.com>
2025-02-20 21:01:00 +0100

    test: Added unit test for purchases retrieval
    -Test for retrieving purchases without filter
    -Test for retrieving purchases by country

--- fastapi/test_main.py
+++ fastapi/test_main.py
@@ -15,16 +15,25 @@ def test_add_purchase():
             "amount": 365.25,
         },
     )
-    print("JSON", response.json())
     assert response.status_code == 200
     assert response.json()["customer_name"] == "John Cruise"
 
 def test_add_bulk_purchases():
     with open(CSV_FILE_PATH, "rb") as file:
         response = client.post("/purchase/bulk/", files={"file": ("sample_purchases.csv", file, "text/csv")})
-    print("Response JSON", response.json())
     assert response.status_code == 200
     assert "added" in response.json()
+
+def test_get_purchases_no_filter():
+    response = client.get("/purchases/")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+def test_get_purchases_by_country():
+    response = client.get("/purchases/?country=United States of America")
+    assert response.status_code == 200
+    assert response.json() != []
+    assert all(purchase["country"] == "United States of America" for purchase in response.json())
 
 def test_calculate_kpis():
     response = client.get("purchases/kpis/")
